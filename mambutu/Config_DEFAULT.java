16440e76aa7b4b539e5028a780dcee2d22829411
stjen <stefan@upx.dk>
2015-12-15 22:44:16 +0100

    Default config file added

---
+++ mambutu/Config_DEFAULT.java
@@ -0,0 +1,37 @@
+package mambutu;
+
+/**
+ * This file is part of Mambutu.
+ * <p/>
+ * Mambutu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Mambutu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with Mambutu.  If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * <p/>
+ * Created by mabool on 11/24/15.
+ */
+public class Config_DEFAULT {
+    // Rename this file to Config.java
+    public static final String PROGRAM_NAME = "Mambutu";
+    public static final String VERSION = "0.02";
+    public static final String BOT_NICK = "THEBOTSNICK";
+    public static final boolean APPEND_NICK_CMD = true;
+    public static final String BOT_REALNAME = "THE REAL NAME";
+    public static final String BOT_USERNAME = "BOTUSERNAME";
+    public static final char COMMAND_CHAR = '.';
+    public static final String CHANNELS = "#YOUR_CHANNEL, #YOUR_OTHER_CHANNEL";
+    public static final boolean AUTO_REJOIN_CHANNEL = true;
+    public static final String SERVER_NAME = "irc.server.foo";
+    public static final int SERVER_PORT = 6667;
+    public static final String LOG_FILE = "main.log";
+    public static final String BUG_LOG_FILE = "bugs.log";
+    public static final String WUNDERGROUND_API_KEY = "YOUR_API_KEY";
+}
